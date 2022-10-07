c461158e63ebb897022e3c30b445eba88b11dcdb
Esat Satan <esatsatan91@gmail.com>
2022-10-07 03:08:57 +0300

    adminLogin integrated.

--- flutter_baibu/lib/adminscreens/AdminDuyuruEkle/duyuruEkle.dart
+++ flutter_baibu/lib/adminscreens/AdminDuyuruEkle/duyuruEkle.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../adminduyuru/duyurularim.dart';
@@ -10,6 +11,18 @@ class duyuruEkle extends StatefulWidget {
 }
 
 class _duyuruEkleState extends State<duyuruEkle> {
+  final _duyurubaslik = TextEditingController();
+  final _duyuruIcerigi = TextEditingController();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future duyuruKaydet(String baslik, String duyuru) async {
+    final yayin = <String, String>{
+      'baslik': baslik,
+      'icerik': duyuru,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,105 +30,110 @@ class _duyuruEkleState extends State<duyuruEkle> {
         title: Text('DUYURU EKLE'),
         backgroundColor: Color(0xff151543),
       ),
-      body: SingleChildScrollView(child:
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 115,
-              width: 115,
-              child: Stack(
-                fit: StackFit.expand,
-                clipBehavior: Clip.none,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 115,
+                width: 115,
+                child: Stack(
+                  fit: StackFit.expand,
+                  clipBehavior: Clip.none,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage:
+                          AssetImage("images/birincilogo_3559696.png"),
+                      backgroundColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 350,
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: TextField(
+                  controller: _duyurubaslik,
+                  minLines: 1,
+                  maxLines: 2,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Baslik Ekle...',
+                    hintStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontFamily: "Mulish-SemiBold",
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 300,
+                width: 350,
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: TextField(
+                  controller: _duyuruIcerigi,
+                  minLines: 1,
+                  maxLines: 25,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Duyurunuzu Yaziniz ...',
+                    hintStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontFamily: "Mulish-SemiBold",
+                    ),
+                  ),
+                ),
+              ),
+              Column(
                 children: [
-                  CircleAvatar(
-                    backgroundImage:
-                        AssetImage("images/birincilogo_3559696.png"),
-                    backgroundColor: Colors.white,
+                  Center(
+                    child: Container(
+                      height: 60,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xff0364f6),
+                      ),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontSize: 15, color: Colors.black),
+                        ),
+                        onPressed: () {
+                          var baslik = _duyurubaslik.text;
+                          var icerik = _duyuruIcerigi.text;
+
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const duyurularim()));
+                        },
+                        child: Text(
+                          'DUYURUNU YAYINLA',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-            Container(
-              height: 50,
-              width: 350,
-              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              padding: EdgeInsets.symmetric(horizontal: 30.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              child: TextField(
-                minLines: 1,
-                maxLines: 2,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Baslik Ekle...',
-                  hintStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontFamily: "Mulish-SemiBold",
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 300,
-              width: 350,
-              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              padding: EdgeInsets.symmetric(horizontal: 30.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              child: TextField(
-                minLines: 1,
-                maxLines: 25,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Duyurunuzu Yaziniz ...',
-                  hintStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontFamily: "Mulish-SemiBold",
-                  ),
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                Center(
-                  child: Container(
-                    height: 60,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Color(0xff0364f6),
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        textStyle:
-                            const TextStyle(fontSize: 15, color: Colors.black),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const duyurularim()));
-                      },
-                      child: Text(
-                        'DUYURUNU YAYINLA',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
       ),
       backgroundColor: const Color(0xff151543),
     );
